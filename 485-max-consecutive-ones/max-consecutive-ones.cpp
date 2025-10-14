8428e57b51a26c1082c3b3c05cfd956e2b8f9762
Sushant-garje <152004564+Sushant-garje@users.noreply.github.com>
2025-10-14 17:16:27 +0530

    Time: 0 ms (100.00%) | Memory: 50.1 MB (67.91%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -4,20 +4,19 @@ public:
         int count = 0;
         int ans = 0;
         int n = nums.size();
-        for(int i=0;i<n;i++){
-
-            if(nums[i]==0){
-                count = 0;
-            }else{
+        for (int i=0;i<n;i++){
+            if(nums[i]==1){
                 count++;
-                if(ans<count){
-                    ans = count;
+                if(count>ans){
+                    ans=count;
                 }
+                
+            }else{
+                count=0;
             }
             
+            
         }
-
-        return ans;
- 
+         return ans;
     }
 };
