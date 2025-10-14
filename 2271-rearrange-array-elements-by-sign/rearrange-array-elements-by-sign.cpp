e04578651b989b1ac018dbdd18d402a53252b185
Sushant-garje <152004564+Sushant-garje@users.noreply.github.com>
2025-10-14 18:39:32 +0530

    Time: 11 ms (32.53%) | Memory: 128.3 MB (78.96%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -9,12 +9,12 @@ public:
         for(int i=0;i<n;i++){
             if(nums[i]>0){
                 ans[pos] =nums[i];
-                pos=pos+2;
+                pos+=2;
                 
 
             }else{
                 ans[neg] = nums[i];
-                neg=neg+2;
+                neg+=2;
             }
              
         }
