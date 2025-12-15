1796a677842d2da3cf60dd4fe143979d254cc120
Sushant-garje <152004564+Sushant-garje@users.noreply.github.com>
2025-12-15 23:52:19 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (5.68%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -4,23 +4,12 @@ public:
         int st =0;
         int end= nums.size()-1;
         int ans = 0;
-
         while(end>=st){
             int mid = end-(end-st)/2;
-
-            if(nums[mid]==target){
-                return mid;
-            }
-            else if(nums[mid]>target){
-                end = mid-1;
-                ans = mid;
-            }
-            else{
-                st = mid+1;
-                ans=mid+1;
-            }
+            if(nums[mid]==target) return mid;
+            if(nums[mid]>target){ end = mid-1; ans = mid;}
+            else{st = mid+1; ans = mid+1;}
         }
-
         return ans;
     }
 };
