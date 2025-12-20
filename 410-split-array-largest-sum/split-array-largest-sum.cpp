c6237626818a94da500843fb0c205fc5df3993d4
Sushant-garje <152004564+Sushant-garje@users.noreply.github.com>
2025-12-20 22:11:42 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (88.78%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    pair<int,int> arrinfo(vector<int> &nums){
+        int n = nums.size();
+        int max = INT_MIN;
+        int sum = 0;
+        for(int i= 0;i<n;i++){
+            sum +=nums[i];
+            if(nums[i]>max) max = nums[i];
+        }
+        return {max,sum};
+    }
+    int splits(vector<int> &nums,int mid,int k){
+        int split = 1;
+        int n = nums.size();
+        int lastpos = 0;
+        for(int i = 0;i<n;i++){
+            if(lastpos+nums[i]<=mid){
+                lastpos+=nums[i];
+            }
+            else{
+                split++;
+                lastpos =nums[i];
+            }
+        }
+
+        return split<=k;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        // sort(nums.begin(),nums.end());
+        // pair<int,int> p = arrinfo(nums);
+        // int sn = p.first;   // max
+        // int en = p.second;  // sum
+        auto [st, en] = arrinfo(nums);
+        while(st<=en){
+            int mid = st+(en-st)/2;
+            if(splits(nums,mid,k)){
+                en = mid-1;
+            }
+            else{
+                st = mid+1;
+            }
+        }
+
+        return st;
+        
+    }
+};
