f803c73ccd8f507dcab2a888590504ffa71c97c7
Sushant-garje <152004564+Sushant-garje@users.noreply.github.com>
2025-12-31 02:09:12 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (96.17%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.cpp
+++ 257-binary-tree-paths/binary-tree-paths.cpp
@@ -2,22 +2,26 @@ class Solution {
 public:
     vector<string> binaryTreePaths(TreeNode* root) {
         vector<string> ans;
-        dfs(root, "", ans);
+        string s ="";
+        path(root ,ans,s);
         return ans;
     }
 
-    void dfs(TreeNode* root, string path, vector<string>& ans) {
-        if (!root) return;
+    void path(TreeNode* root, vector<string>& ans, string& s) {
+    if (!root) return;
 
-        path += to_string(root->val);
+    int len = s.size();   // save current length
 
-        if (!root->left && !root->right) {
-            ans.push_back(path);
-            return;
-        }
+    s += to_string(root->val);
 
-        path += "->";
-        dfs(root->left, path, ans);
-        dfs(root->right, path, ans);
+    if (!root->left && !root->right) {
+        ans.push_back(s);
+    } else {
+        s += "->";
+        path(root->left, ans, s);
+        path(root->right, ans, s);
     }
+
+    s.resize(len);  // backtrack safely
+}
 };
