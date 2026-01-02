bb9ec9fca88188359e7ba0107398833482d9bc56
Sushant-garje <152004564+Sushant-garje@users.noreply.github.com>
2026-01-02 20:56:44 +0530

    Time: 10 ms (10.38%) | Memory: 15.5 MB (50.50%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -7,46 +7,39 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-
 class Solution {
 public:
-    void track_par(TreeNode* root, unordered_map<TreeNode*, TreeNode*>& par) {
+    void track_par(TreeNode* root,unordered_map<TreeNode*,TreeNode*> &par){
         queue<TreeNode*> q;
         q.push(root);
-
-        while (!q.empty()) {
-            TreeNode* temp = q.front();
+        while(!q.empty()){
+            
+            TreeNode* temp =q.front();
             q.pop();
-
-            if (temp->left) {
+            if(temp->left) {
                 par[temp->left] = temp;
                 q.push(temp->left);
             }
-            if (temp->right) {
+            if(temp->right){
                 par[temp->right] = temp;
-                q.push(temp->right);
+                 q.push(temp->right);
             }
+            
         }
+
     }
-
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        if (k == 0) return {target->val};
-
-        unordered_map<TreeNode*, TreeNode*> par;
-        track_par(root, par);
-
+        vector<int> ans;
+        unordered_map<TreeNode*,TreeNode*> par;
+        track_par(root,par);
         unordered_set<TreeNode*> visited;
         queue<TreeNode*> q;
-
         q.push(target);
         visited.insert(target);
-
-        int level = 0;
-
-        while (!q.empty()) {
+        int level =0;
+        while(!q.empty()){
             int size = q.size();
-            if (level == k) break;
-
+            if(level==k) break;
             for (int i = 0; i < size; i++) {
                 TreeNode* temp = q.front();
                 q.pop();
@@ -55,26 +48,26 @@ public:
                     visited.insert(temp->left);
                     q.push(temp->left);
                 }
-
                 if (temp->right && !visited.count(temp->right)) {
                     visited.insert(temp->right);
                     q.push(temp->right);
                 }
-
                 if (par.count(temp) && !visited.count(par[temp])) {
                     visited.insert(par[temp]);
                     q.push(par[temp]);
                 }
             }
             level++;
+
         }
 
-        vector<int> ans;
-        while (!q.empty()) {
+        while(!q.empty()){
             ans.push_back(q.front()->val);
             q.pop();
         }
 
         return ans;
+        
     }
+
 };
