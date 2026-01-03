36a8cef1582f894dd408d891904c77fed4e87711
Sushant-garje <152004564+Sushant-garje@users.noreply.github.com>
2026-01-03 13:15:11 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (72.21%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -11,22 +11,52 @@
  */
 class Solution {
 public:
-    vector<int> preorderTraversal(TreeNode* root) {
-        if(!root) return {};
+        vector<int> preorderTraversal(TreeNode* root) {
         vector<int> ans;
-        stack<TreeNode*> q;
-        q.push(root);
-        while(!q.empty()){
-            TreeNode* temp = q.top();
-            q.pop();
-            ans.push_back(temp->val);
-            if(temp->right) q.push(temp->right);
-            if(temp->left)  q.push(temp->left);
-            
+        if(!root) return ans;
+        TreeNode* curr = root;
+        while(curr){
+            if(!(curr->left)){
+                ans.push_back(curr->val);
+                curr = curr->right;
+            }
+            else{
+                TreeNode* prev = curr->left;
+                while(prev->right && prev->right!=curr){
+                    prev = prev->right;
+                }
+
+                if(prev->right==nullptr){
+                    prev->right = curr;
+                    ans.push_back(curr->val);
+                    curr = curr->left;
+                }
+                else{
+                    prev->right = nullptr;
+                    curr= curr->right;
+                }
+
+            }
         }
-        return ans;
         
+        return ans;
     }
+    // vector<int> preorderTraversal(TreeNode* root) {
+    //     if(!root) return {};
+    //     vector<int> ans;
+    //     stack<TreeNode*> q;
+    //     q.push(root);
+    //     while(!q.empty()){
+    //         TreeNode* temp = q.top();
+    //         q.pop();
+    //         ans.push_back(temp->val);
+    //         if(temp->right) q.push(temp->right);
+    //         if(temp->left)  q.push(temp->left);
+            
+    //     }
+    //     return ans;
+        
+    // }
     // void preorder(vector<int> &ans,TreeNode* root){
     //     if(root==nullptr) return;
     //     ans.push_back(root->val);
